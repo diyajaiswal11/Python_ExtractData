9d7440aa022ab90727f1dafddcf1101b0823b36c
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-07-01 15:57:26 +0530

    initial commit

--- extract.py
+++ extract.py
@@ -1,6 +1,16 @@
-print("Hiii")
 import PyPDF2 as p2
-PDFfile=open('AGK.pdf','rb')
-pdfread=p2.PdfFileReader(PDFfile)
-x=pdfread.getPage(2)
+
+pdffile=open('AGK.pdf','rb')
+pdfread=p2.PdfFileReader(pdffile)
+
+#TO EXTRACT SINGLE PAGE DATA
+"""x=pdfread.getPage(10)
 print(x.extractText())
+"""
+
+#TO EXTRACT ENTIRE PDF'S DATA
+i=0
+while i<pdfread.getNumPages():
+    pginfo=pdfread.getPage(i)
+    print(pginfo.extractText())
+    i=i+1
