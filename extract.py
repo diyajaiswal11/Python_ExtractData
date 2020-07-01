63a3da0f454e2fd289faa95bc413bc6667fac340
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-07-01 16:15:00 +0530

    initial commit:

--- extract.py
+++ extract.py
@@ -4,11 +4,13 @@ pdffile=open('AGK.pdf','rb')
 pdfread=p2.PdfFileReader(pdffile)
 
 #TO EXTRACT SINGLE PAGE DATA
-"""x=pdfread.getPage(10)
+"""
+x=pdfread.getPage(10)
 print(x.extractText())
 """
 
 #TO EXTRACT ENTIRE PDF'S DATA
+
 i=0
 while i<pdfread.getNumPages():
     pginfo=pdfread.getPage(i)
